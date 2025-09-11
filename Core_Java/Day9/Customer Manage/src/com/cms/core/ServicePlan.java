fcc884de7d4889f4c29464a3937dfdb63135667d
dac <dac@dypm.org>
2025-09-11 15:51:19 +0530

    Day 5 to 9

---
+++ Core_Java/Day9/Customer Manage/src/com/cms/core/ServicePlan.java
@@ -0,0 +1,12 @@
+package com.cms.core;
+
+public enum ServicePlan {
+	SILVER(1000),
+	GOLD(2000),
+	DIAMOND(5000),
+	PLATINUM(10000);
+
+	ServicePlan(int i) {
+		// TODO Auto-generated constructor stub
+	}	
+}
