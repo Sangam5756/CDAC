78abe4721927b2bc9ace529f1b29684b8f6715e9
Sangam5756 <sangammunde3@gmail.com>
2025-09-23 07:34:36 +0530

    BackeryManageMent Practice Done

---
+++ C_CPP_LAB/Practice/BakeryManagementSystem/ChocolateCake.h
@@ -0,0 +1,35 @@
+#ifndef CHOCOLATECAKE_H
+#define CHOCOLOLATE_H
+#include "Cake.h"
+#include <iostream>
+using namespace std;
+
+class ChocolateCake : public Cake{
+    string chocolateType;
+    public:
+        ChocolateCake(string cakeName,double price,string chocolateType):Cake(cakeName,price){
+            this->chocolateType=chocolateType;
+        }
+        double calculateChocolatePrice(){
+            double basePrice =getPrice();
+            // if fudge then 5% extra
+            // if black forest 10 % extra
+            if(chocolateType == "Fudge"){
+                basePrice =(getPrice() +(getPrice()*0.05));
+            }
+             if(chocolateType == "BlackForest"){
+                basePrice  =(getPrice() +(getPrice()*0.10));
+            }
+            return basePrice;
+
+        }
+
+        void display(){
+            Cake::display();
+            cout<<"Chocolate Type : "<<chocolateType<<endl;
+        }
+
+};
+
+
+#endif
